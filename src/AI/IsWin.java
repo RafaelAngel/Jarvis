0aaa079051167fe4cdec1573347197b8e5476a33
David Clarke <daclarke@ugls1.ece>
2013-05-21 13:32:55 -0700

    changes for iswin submission

--- src/AI/IsWin.java
+++ src/AI/IsWin.java
@@ -1,4 +1,8 @@
-package AI;
+//package AI;
+
+/*
+* non-blocking console input courtesy of http://www.darkcoding.net/software/non-blocking-console-io-is-not-possible/
+*/
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,6 +10,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.ByteArrayOutputStream; 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -23,7 +29,7 @@ public class IsWin {
     private static final byte gamePiece_g = new Byte((byte) 10);
     private static final byte gamePiece_s = new Byte((byte) 11);
     
-    public static void main(String[] args) throws IOException {
+    /*public static void main(String[] args) throws IOException {
         
         IsWin isWin = new IsWin();
         String input = "";         
@@ -42,6 +48,97 @@ public class IsWin {
         log.write(score);
         System.out.println(score);
         log.flush();
+    }*/
+	
+
+    private static String ttyConfig;
+
+    public static void main(String[] args) {
+
+	String input = "";
+        IsWin isWin = new IsWin();
+
+	try {
+		setTerminalToCBreak();
+		char character = (char) System.in.read();
+		while (character != ')') {
+			if ( System.in.available() != 0 ) {
+				input += character;
+				character = (char) System.in.read();
+			}
+		} // end while
+	}
+	catch (IOException e) {
+		System.err.println("IOException");
+	}
+	catch (InterruptedException e) {
+		System.err.println("InterruptedException");
+	}
+	finally {
+		try {
+			stty( ttyConfig.trim() );
+		}
+		catch (Exception e) {
+			System.err.println("Exception restoring tty config");
+		}
+        }
+        input = input.substring(1);   
+	int score = isWin.winFunction(input);
+	System.out.println(score);
+    }
+
+    private static void setTerminalToCBreak() throws IOException, InterruptedException {
+
+        ttyConfig = stty("-g");
+
+        // set the console to be character-buffered instead of line-buffered
+        stty("-icanon min 1");
+
+        // disable character echoing
+        stty("-echo");
+    }
+
+    /**
+     *  Execute the stty command with the specified arguments
+     *  against the current active terminal.
+     */
+    private static String stty(final String args)
+                    throws IOException, InterruptedException {
+        String cmd = "stty " + args + " < /dev/tty";
+
+        return exec(new String[] {
+                    "sh",
+                    "-c",
+                    cmd
+                });
+    }
+
+    /**
+     *  Execute the specified command and return the output
+     *  (both stdout and stderr).
+     */
+    private static String exec(final String[] cmd)
+                    throws IOException, InterruptedException {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+
+        Process p = Runtime.getRuntime().exec(cmd);
+        int c;
+        InputStream in = p.getInputStream();
+
+        while ((c = in.read()) != -1) {
+            bout.write(c);
+        }
+
+        in = p.getErrorStream();
+
+        while ((c = in.read()) != -1) {
+            bout.write(c);
+        }
+
+        p.waitFor();
+
+        String result = new String(bout.toByteArray());
+        return result;
     }
     
     public int winFunction(String input) {
