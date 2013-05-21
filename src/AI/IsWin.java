7caab3482de4c1d5ad770c042544519fb49fbd84
unknown <daclarke@a5.seng.uvic.ca>
2013-05-21 11:59:10 -0700

    changes for IsWin submission

--- src/AI/IsWin.java
+++ src/AI/IsWin.java
@@ -1,8 +1,12 @@
 package AI;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +22,28 @@ public class IsWin {
     private static final byte gamePiece_b = new Byte((byte) 01);
     private static final byte gamePiece_g = new Byte((byte) 10);
     private static final byte gamePiece_s = new Byte((byte) 11);
+    
+    public static void main(String[] args) throws IOException {
+        
+        IsWin isWin = new IsWin();
+        String input = "";         
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int nextChar = br.read();
+        
+        while (nextChar != -1 && ')' != (char) nextChar) {
+            input += (char) nextChar;
+            nextChar = br.read();
+        }  
+        input = input.substring(1);        
+        int score = isWin.winFunction(input);        
+        BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        log.write(score);
+        System.out.println(score);
+        log.flush();
+    }
+    
     public int winFunction(String input) {
 
         initializeMaps();
@@ -39,84 +64,84 @@ public class IsWin {
             Arrays.fill(column, gamePiece_s);
         }
 
+        ArrayList<int[]> activePieces = new ArrayList<int[]>();        
+        
         for (int x = 0; x < num_col; x++) {
             for (int y = 0; y < num_row; y++) {
                 gameBoard[x + 3][y + 3] = pieceMap.get((String) tokenizer.nextElement());
-                if(gameBoard[x + 3][y + 3] == gamePiece_s){
+                if(gameBoard[x + 3][y + 3] != gamePiece_s){
+                    int[] piece = {x,y};
+                    activePieces.add(piece);
                 }
             }
         }
 
         // (7,6,3,600,290,20,r,b,s,s,s,s,b,b,r,g,s,s,b,r,b,b,g,s,r,g,r,b,s,s,r,r,b,r,r,s,s,s,s,s,s,s,s,s,s,s,s,s)
 
-/*        for (int x = 0; x < num_col; x++) {
+       /* for (int x = 0; x < num_col; x++) {
             for (int y = 0; y < num_row; y++) {
                 System.out.print(getKeyByValue(pieceMap, gameBoard[x + 3][y + 3]));
             }
             System.out.println();
         }*/
 
-        int score = calculateScore(gameBoard, num_col, num_row);
-
-       /* try {
-            BufferedWriter log = new BufferedWriter(new OutputStreamWriter(System.out));
-            log.write(String.valueOf(score));
-            log.flush();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
+        int score = calculateScore(gameBoard, num_col, num_row, activePieces);
 
         return score;
     }
 
-    private int calculateScore(byte[][] gameBoard,int num_col,int num_row) {
+    private int calculateScore(byte[][] gameBoard, int num_col, int num_row, ArrayList<int[]> activePieces) {
         
         int blueScore_total = 0;
         int redScore_total = 0;
         int blueScore_highest = 0;
         int redScore_highest = 0;
         
-        for (int x = 0; x < num_col; x++) {
-            for (int y = 0; y < num_row; y++) {
-                
-                Byte[][] possibleWins = new Byte[13][4]; 
-
-                // Horizontal wins
-                possibleWins[0] = new Byte[] { gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 3], gameBoard[x + 1][y + 3], gameBoard[x][y + 3] };
-                possibleWins[1] = new Byte[] { gameBoard[x + 4][y + 3], gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 3], gameBoard[x + 1][y + 3] };
-                possibleWins[2] = new Byte[] { gameBoard[x + 5][y + 3], gameBoard[x + 4][y + 3], gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 3] };
-                possibleWins[3] = new Byte[] { gameBoard[x + 6][y + 3], gameBoard[x + 5][y + 3], gameBoard[x + 4][y + 3], gameBoard[x + 3][y + 3] };
-                
-                // Diagonal wins
-                possibleWins[4] = new Byte[] { gameBoard[x + 6][y + 6], gameBoard[x + 5][y + 5], gameBoard[x + 4][y + 4], gameBoard[x + 3][y + 3] };
-                possibleWins[5] = new Byte[] { gameBoard[x + 5][y + 5], gameBoard[x + 4][y + 4], gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 2] };
-                possibleWins[6] = new Byte[] { gameBoard[x + 4][y + 4], gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 2], gameBoard[x + 1][y + 1] };
-                possibleWins[7] = new Byte[] { gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 2], gameBoard[x + 1][y + 1], gameBoard[x][y] };
-                possibleWins[8] = new Byte[] { gameBoard[x][y + 6], gameBoard[x + 1][y + 5], gameBoard[x + 2][y + 4], gameBoard[x + 3][y + 3] };
-                possibleWins[9] = new Byte[] { gameBoard[x + 1][y + 5], gameBoard[x + 2][y + 4], gameBoard[x + 3][y + 3], gameBoard[x + 4][y + 2] };
-                possibleWins[10] = new Byte[] { gameBoard[x + 2][y + 4], gameBoard[x + 3][y + 3], gameBoard[x + 4][y + 2], gameBoard[x + 5][y + 1] };
-                possibleWins[11] = new Byte[] { gameBoard[x + 3][y + 3], gameBoard[x + 4][y + 2], gameBoard[x + 5][y + 1], gameBoard[x + 6][y] };
-                
-                // Vertical wins
-                possibleWins[12] = new Byte[] { gameBoard[x + 3][y + 3], gameBoard[x + 3][y + 2], gameBoard[x + 3][y + 1], gameBoard[x + 3][y] };
-                       
-                for(Byte[] possibleWin: possibleWins){
-                    if(winMap.get(Arrays.toString(possibleWin)) != null){
-                        if(winMap.get(Arrays.toString(possibleWin)) > 0){
-                            blueScore_total += winMap.get(Arrays.toString(possibleWin)); 
-                            if(blueScore_highest < winMap.get(Arrays.toString(possibleWin))){
-                                blueScore_highest = winMap.get(Arrays.toString(possibleWin));
-                            }
-                        }else if(winMap.get(Arrays.toString(possibleWin)) < 0){
-                            redScore_total -= winMap.get(Arrays.toString(possibleWin));      
-                            if(redScore_highest > winMap.get(Arrays.toString(possibleWin))){
-                                redScore_highest = winMap.get(Arrays.toString(possibleWin));
-                            }               
+        int x;
+        int y;
+        
+        for(int[] piece: activePieces){            
+            x = piece[0];
+            y = piece[1];
+        
+            Byte[][] possibleWins = new Byte[13][4]; 
+            
+            // Horizontal wins
+            possibleWins[0] = new Byte[] { gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 3], gameBoard[x + 1][y + 3], gameBoard[x][y + 3] };
+            possibleWins[1] = new Byte[] { gameBoard[x + 4][y + 3], gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 3], gameBoard[x + 1][y + 3] };
+            possibleWins[2] = new Byte[] { gameBoard[x + 5][y + 3], gameBoard[x + 4][y + 3], gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 3] };
+            possibleWins[3] = new Byte[] { gameBoard[x + 6][y + 3], gameBoard[x + 5][y + 3], gameBoard[x + 4][y + 3], gameBoard[x + 3][y + 3] };
+            
+            // Diagonal wins
+            possibleWins[4] = new Byte[] { gameBoard[x + 6][y + 6], gameBoard[x + 5][y + 5], gameBoard[x + 4][y + 4], gameBoard[x + 3][y + 3] };
+            possibleWins[5] = new Byte[] { gameBoard[x + 5][y + 5], gameBoard[x + 4][y + 4], gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 2] };
+            possibleWins[6] = new Byte[] { gameBoard[x + 4][y + 4], gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 2], gameBoard[x + 1][y + 1] };
+            possibleWins[7] = new Byte[] { gameBoard[x + 3][y + 3], gameBoard[x + 2][y + 2], gameBoard[x + 1][y + 1], gameBoard[x][y] };
+            possibleWins[8] = new Byte[] { gameBoard[x][y + 6], gameBoard[x + 1][y + 5], gameBoard[x + 2][y + 4], gameBoard[x + 3][y + 3] };
+            possibleWins[9] = new Byte[] { gameBoard[x + 1][y + 5], gameBoard[x + 2][y + 4], gameBoard[x + 3][y + 3], gameBoard[x + 4][y + 2] };
+            possibleWins[10] = new Byte[] { gameBoard[x + 2][y + 4], gameBoard[x + 3][y + 3], gameBoard[x + 4][y + 2], gameBoard[x + 5][y + 1] };
+            possibleWins[11] = new Byte[] { gameBoard[x + 3][y + 3], gameBoard[x + 4][y + 2], gameBoard[x + 5][y + 1], gameBoard[x + 6][y] };
+            
+            // Vertical wins
+            possibleWins[12] = new Byte[] { gameBoard[x + 3][y + 3], gameBoard[x + 3][y + 2], gameBoard[x + 3][y + 1], gameBoard[x + 3][y] };
+                   
+            for(Byte[] possibleWin: possibleWins){
+                Integer score = winMap.get(Arrays.toString(possibleWin));
+                if(score != null){
+                    if(score > 0){
+                        blueScore_total += score; 
+                        if(blueScore_highest < score){
+                            blueScore_highest = score;
                         }
-                        gameBoard[x+3][y+3] = gamePiece_s;
+                    }else if(score < 0){
+                        redScore_total -= score;      
+                        if(redScore_highest > score){
+                            redScore_highest = score;
+                        }               
                     }
-                }                
-            }
+                    gameBoard[x+3][y+3] = gamePiece_s;
+                }
+            }                
         }
         
         // No current win condition (game still in progress)
--- src/AI/Jarvis.java
+++ src/AI/Jarvis.java
@@ -1,4 +1,4 @@
-package AI;
+/*package AI;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -8,13 +8,15 @@ public class Jarvis {
 
     public static void main(String[] args) throws IOException {
 
+        long startTime = System.nanoTime();
+        
         IsWin isWin = new IsWin();
         
         System.out.println("Hello World");
 
         String input = ""; 
         
-        BufferedReader br = new BufferedReader(new FileReader("testData.txt"));    
+        BufferedReader br = new BufferedReader(new FileReader("testData2.txt"));    
         
         int count = 0;
         
@@ -28,14 +30,18 @@ public class Jarvis {
             
             String actual = br.readLine();
             if(actual.contains(String.valueOf(isWin.winFunction(input))) == false){
-                System.out.println(isWin.winFunction(input) + " " + actual + " : " + input + " ;  successful:" + count);
+                //System.out.println(isWin.winFunction(input) + " " + actual + " : " + input + " ;  successful:" + count);
             }else{
                 count++;
             }
             input = "";
-        }
+        }        
+
+        long endTime = System.nanoTime();
         
-        System.out.println("Successful wins: " + count);
+        System.out.println("Successful wins: " + count + " , running time:" + (endTime - startTime));
+        
     }
 
 }
+*/
